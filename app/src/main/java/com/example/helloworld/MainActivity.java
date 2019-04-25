fea68505ed8c81e2966f9910e58b42372154215d
anikolic1 <42483909+anikolic1@users.noreply.github.com>
2019-04-25 13:02:26 -0700

    Added a text recognition feature and a sercret button

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -5,24 +5,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.helloworld.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.button);
+        // Add a click through Java instead of xml
+        /*
+        Button button = findViewById(R.id.button_secret);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goToSecondActivity();
+                goToSecretActivity();
             }
         });
+        */
     }
 
-    private void goToSecondActivity(){
+    // Called when user taps the send button
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    // Called when user taps the secret button
+    public void goToSecretActivity(View view){
         Intent intent = new Intent(this, SecondActivity.class);
         startActivity(intent);
     }
