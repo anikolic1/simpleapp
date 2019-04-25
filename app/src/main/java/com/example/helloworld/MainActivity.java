b258504fe6baaf8996e12651b9cdb567987db04a
anikolic1 <42483909+anikolic1@users.noreply.github.com>
2019-04-25 05:01:28 -0700

    Initial commit with the working project

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.helloworld;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSecondActivity();
+            }
+        });
+    }
+
+    private void goToSecondActivity(){
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+}
